c9ba8467efc228e14e733d715a7437d211278eea
Mohit Sagar <mohitsagar378@gmail.com>
2026-01-17 09:58:48 +0530

    Initial commit

---
+++ src/main/java/com/Telusko/LearningSpringBoot/Hello.java
@@ -0,0 +1,12 @@
+package com.Telusko.LearningSpringBoot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/Telusko/LearningSpringBoot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Telusko.LearningSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

32e2632ad2e217023749a0512aa1b3db881dfa61
Mohit Sagar <mohitsagar378@gmail.com>
2026-01-17 10:25:24 +0530

    Autowire , Qualifier , primary

--- src/main/java/com/Telusko/LearningSpringBoot/Alien.java
+++ src/main/java/com/Telusko/LearningSpringBoot/Alien.java
@@ -1,11 +1,25 @@
 package com.Telusko.LearningSpringBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
 
+    @Autowired // field injection
+    private Laptop obj;
+
+//    public Alien(Laptop obj) {
+//        this.obj = obj; // constructor injection
+//    }
+
+//    @Autowired
+//    public void setter(Laptop obj) {
+//        this.obj = obj; // setter injection
+//    }
+
     public void build(){
+        obj.Compile();
         System.out.println("Chal Gaya");
     }
 }
---
+++ src/main/java/com/Telusko/LearningSpringBoot/Laptop.java
@@ -0,0 +1,11 @@
+package com.Telusko.LearningSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void Compile(){
+        System.out.println("---------------------------Compiling--------------------------------");
+    }
+}
